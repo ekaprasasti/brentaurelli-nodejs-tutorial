befcd1db177a35ca72540f7e196da67354583fa1
eka prasasti <ekaputraprasasti@gmail.com>
2016-10-16 08:40:43 +0700

    tutorial 3 - express & morgan

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.use('/', function(req, res){
+	res.send('Our First Express Pragram!');
+});
+
+app.listen(port);
+console.log('Server running on port ' + port);
